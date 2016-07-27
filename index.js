72c0bb5a008e090b2946055f882c971002143fbd
Kurtis Kemple <kurtiskemple@gmail.com>
2016-07-27 09:19:15 -0700

    update to use new colors object structure

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
-'use strict';
+'use strict'
 
-const foregroundColor = '#eff0eb';
-const backgroundColor = '#24242e';
-const black = backgroundColor;
-const slate = '#5b5a77';
-const lightSlate = '#767594';
-const blue = '#767594';
-const orange = '#df671f';
-const lightOrange = '#ff8c4a';
-const gray = '#6a574c';
-const brightBlack = '#6a574c';
-const brightWhite = foregroundColor;
+const foregroundColor = '#eff0eb'
+const backgroundColor = '#24242e'
+const black = backgroundColor
+const slate = '#5b5a77'
+const lightSlate = '#767594'
+const blue = '#767594'
+const orange = '#df671f'
+const lightOrange = '#ff8c4a'
+const gray = '#6a574c'
+const brightBlack = '#6a574c'
+const brightWhite = foregroundColor
+
+const colors = {
+	black: black,
+	red: slate,
+	green: lightSlate,
+	yellow: blue,
+	blue: orange,
+	magenta: lightOrange,
+	cyan: gray,
+	white: foregroundColor,
+	lightBlack: brightBlack,
+	lightRed: slate,
+	lightGreen: lightSlate,
+	lightYellow: blue,
+	lightBlue: orange,
+	lightMagenta: lightOrange,
+	lightCyan: gray,
+	lightWhite: foregroundColor
+}
 
 exports.decorateConfig = config => {
 	return Object.assign({}, config, {
 		backgroundColor,
 		foregroundColor,
 		borderColor: '#222430',
-		cursorColor: '#df671f',
-		colors: [
-			black,
-			slate,
-      lightSlate,
-      blue,
-			orange,
-			lightOrange,
-			gray,
-
-			// bright
-			brightBlack,
-			slate,
-      lightSlate,
-      blue,
-			orange,
-			lightOrange,
-			brightWhite
-		],
+		cursorColor: orange,
+		colors: colors,
 		css: `
 			${config.css || ''}
 			.tab_active:before {
-				border-color: rgba(223, 103, 31, 0.25);
+				border-color: rgba(223, 103, 31, 0.25)
 			}
 		`
-	});
-};
+	})
+}
+
+exports.middleware = () => (next) => (action) => {
+  switch (action.type) {
+    case 'CONFIG_LOAD':
+    case 'CONFIG_RELOAD':
+      action.config.foregroundColor = foregroundColor
+      action.config.backgroundColor = backgroundColor
+      action.config.cursorColor = orange
+      action.config.colors = colors
+  }
+  next(action)
+}
